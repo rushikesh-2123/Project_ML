c537ea71c3a527bfa17ea8f347532011a5184e0d
rushikesh-2123 <rushikeshfegade44@gmail.com>
2024-10-05 17:50:38 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT='=e .'
+HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
@@ -13,7 +13,8 @@ def get_requirements(file_path:str)->List[str]:
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-
+    
+    return requirements
 
 setup(
     name='projectml',
